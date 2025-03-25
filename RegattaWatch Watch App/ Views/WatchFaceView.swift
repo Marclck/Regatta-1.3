69761b2a90529628376cce824d46ca74c6098fea
marclck <marclck@me.com>
2025-03-25 21:39:57 +0000

    redudant code

--- RegattaWatch Watch App/ Views/WatchFaceView.swift
+++ RegattaWatch Watch App/ Views/WatchFaceView.swift
@@ -64,17 +64,6 @@ struct WatchFaceView: View {
                             }
                     }
                     
-                    if !cruisePlanState.isActive {
-                        Text(settings.teamName)
-                            .font(.system(size: 11, weight: .semibold))
-                            .rotationEffect(.degrees(270), anchor: .center)
-                            .foregroundColor(Color(hex: settings.teamNameColorHex).opacity(1))
-                            .position(x: 4, y: centerY/2+60)
-                            .onReceive(timeTimer) { input in
-                                currentTime = input
-                            }
-                    }
-                    
                     // Content
                     VStack(spacing: 0) {
                         // Timer display instead of current time
